4a934a00234e8181e05898e4eb80a2b8ebc8541f
tonatiuh <tonatiuhN@hotmail.es>
2012-06-23 21:46:00 -0500

    order_item model with relations and validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,6 @@
 class OrderItem < ActiveRecord::Base
   attr_accessible :product_id, :order_id, :quantity
+  validates_presence_of :order_id, :product_id
+  belongs_to :order
+  belongs_to :product
 end
