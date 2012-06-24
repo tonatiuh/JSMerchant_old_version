6e5bd5906c89b316e22508c9166e726bfc580ca0
tonatiuh <tonatiuhN@hotmail.es>
2012-06-23 21:54:26 -0500

    add to cart link added to the product list and other relations on order and product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ActiveRecord::Base
   attr_accessible :customer_id, :status
+  has_many :order_items
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :price, :description, :image_url, :stock
   validates_numericality_of :stock, :only_integer => 0, :greater_than => 0
+  has_many :order_items
   def price=(input)
     input.delete!("$")
     super
