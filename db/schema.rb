1cf1288daa11dfb8df7ff3e0942e14c36c7cbf21
tonatiuh <tonatiuh@tangosource.com>
2012-07-03 21:58:41 -0500

    changes to set 0 as default for each new OrderItem

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120624023218) do
+ActiveRecord::Schema.define(:version => 20120704024952) do
 
   create_table "order_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "order_id"
-    t.integer  "quantity"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "quantity",   :default => 0
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "orders", :force => true do |t|
